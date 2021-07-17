9c16920a7a66b3f0577c823fe604f104135f0053
Amurph733 <amurph733@gmail.com>
2021-07-17 16:04:45 -0400

    Initial Commit

---
+++ GitHubTest003/GitHubTest003App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTest003App.swift
+//  GitHubTest003
+//
+//  Created by Aidan Murphy on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTest003App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
